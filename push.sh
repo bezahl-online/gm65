9f0bc2653c14f988e4bd930c9e3c4a0cde9a8d08
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-12-18 07:24:58 +0100

    added version file

--- push.sh
+++ push.sh
@@ -2,5 +2,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-if [[ $(uname -m)!="armv7" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
-(./build.sh && docker push www.greisslomat.at:5000/gm65:1.0 && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+version=$(cat version)
+echo "building version ${version}"
+if [[ $(uname -m)!="armv7" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; fi
+(./build.sh && docker push www.greisslomat.at:5000/gm65:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
