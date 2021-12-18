aea3599c0f2b5a1b5e5ade57e063fd1b30b1cfb9
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-12-18 07:33:33 +0100

    .

--- push.sh
+++ push.sh
@@ -4,5 +4,5 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 version=$(cat version)
 echo "building version ${version}"
-if [[ $(uname -m)!="armv7" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; fi
+if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
 (./build.sh && docker push www.greisslomat.at:5000/gm65:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
