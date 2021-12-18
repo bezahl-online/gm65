3b55f2b1823af9d429695a777a08493a324c0f90
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-12-18 07:17:41 +0100

    dont push other than armv7 builds

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+if [[ $(uname -m)!="armv7" ]]; then echo -e "\n${RED}ERROR: build need to be done on 'ARMV7' plattform${NC}\n"; exit 1; fi
 (./build.sh && docker push www.greisslomat.at:5000/gm65:1.0 && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
