70fc3b14284c3055ec5f98ecbebb1cdbf0a41ea7
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-04-02 15:26:17 +0200

    build with version and arch

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+git pull
 version=$(cat version)
-echo "building version ${version}"
-if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
+echo "building version $(arch)_${version}"
+# if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
 (./build.sh && docker push www.greisslomat.at:5000/gm65:${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
