4380f6d67279d8769072ed0d5b9e1b94215f4aac
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2022-05-21 22:47:37 +0200

    new port

--- push.sh
+++ push.sh
@@ -2,8 +2,14 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-git pull
+git pull 
+REPRO="www.greisslomat.at:3307"
 version=$(cat version)
+PRG="gm65"
 echo "building version $(arch)_${version}"
-# if [[ $(uname -m) != "armv7l" ]]; then echo -e "\n${RED}build need to be done on armv7 plattform${NC}\n"; exit 1; fi
-(./build.sh && docker push www.greisslomat.at:5000/gm65:$(arch)_${version} && echo -e "\n${GREEN}push successfull${NC}\n") || echo -e "\n${RED}push failed${NC}\n"
+docker login https://www.greisslomat.at:3307 --username ralph --password natural-Kennwort
+(./build.sh && docker push $REPRO/$PRG:$(arch)_${version} && echo -e "\n${GREEN}build successfull${NC}\n") || echo -e "\n${RED}build failed${NC}\n"
+if [ "${1}" == "latest" ]; then
+    docker tag $REPRO/$PRG:$(arch)_${version} $REPRO/$PRG:$(arch)_latest
+    docker push $REPRO/$PRG:x86_64_latest
+fi
